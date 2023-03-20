b473992b5e5ebb5e550f052be90f12caa23a66b2
Elena Teaca <ririteaca@gmail.com>
2023-03-20 20:39:27 +0200

    logic correction

--- robot.js
+++ robot.js
@@ -1,47 +1,52 @@
 function Move(instructions){
     for(const ins of instructions){
-            if(ins==="L" && (position[2]==="N" || position[2]==="S")){
-                position[2]="V";
+        if ( ins==="L" && position[2]==="N"){
+            position[2]="V"; continue;
         }
-            if(ins==="R" && (position[2]==="N" || position[2]==="S")){
-                position[2]="E";
-            }
-             if(ins==="R" && position[2]==="E"){
-                position[2]="S";
-            }
-            if(ins==="R" && position[2]==="V"){
-                position[2]="N";
-         }
-         if(ins==="L" && position[2]==="E"){
-                position[2]="N";
-            }
-            if(ins==="L" && position[2]==="V"){
-                position[2]="S";
-         }
+        if ( ins==="L" && position[2]==="S"){
+            position[2]="E"; continue;
+        }
+        if( ins==="R" && position[2]==="N" ){
+            position[2]="E"; continue;
+        }
+        if ( ins==="R" && position[2]==="S"){
+            position[2]="V"; continue;
+        }            
+        if( ins==="R" && position[2]==="E"){
+            position[2]="S"; continue;
+        }
+        if( ins==="R" && position[2]==="V"){
+            position[2]="N"; continue;
+        }
+        if( ins==="L" && position[2]==="E"){
+            position[2]="N"; continue;
+        }
+        if(ins==="L" && position[2]==="V"){
+            position[2]="S"; continue;
+        }
 
-         if(ins==="M"){
-            if(position[2]==="N"){
-                position[1]+=1;
+        if ( ins==="M" ){
+            if (position[2] === "N"){
+                position[1] += 1; continue;
             }
-             if(position[2]==="S"){
-                position[1]-=1;
+            if (position[2] === "S"){
+                position[1] -=1 ; continue;
             }
-             if(position[2]==="E"){
-                position[0]+=1;
+            if (position[2] === "E"){
+                position[0] +=1; continue;
             }
-            if(position[2]==="V"){
-                position[0]-=1;
+            if (position[2] === "V"){
+                position[0] -= 1; continue;
             }
-         }
+        }
     }
     return position;
 }
 
-// let mapDim = [5, 5], 
 position = [1, 2, "N"];
 
-const dimPlatou={
-    startPos:[0,0],
-    endPos:[5,5]
-};
+// const dimPlatou={
+//     startPos:[0,0],
+//     endPos:[5,5]
+// };
 console.log(Move("LMLMLMLMM"));
